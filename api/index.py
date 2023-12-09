c89e8014ed56611df01f03c93fd1cc6d114c637d
Max <m@xtyrrell.com>
2023-12-09 21:17:35 +0200

    feat: return upload feedback

--- api/index.py
+++ api/index.py
@@ -1,4 +1,3 @@
-from flask import Flask
 import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
@@ -32,18 +31,17 @@ def upload_file():
     if request.method == "POST":
         # check if the post request has the file part
         if "file" not in request.files:
-            flash("No file part")
-            return redirect(request.url)
+            return "No file part"
         file = request.files["file"]
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == "":
-            flash("No selected file")
-            return redirect(request.url)
+            return "No selected file"
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename or "example")
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            return redirect(url_for("download_file", name=filename))
+            # filename = secure_filename(file.filename or "example")
+            # file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            # return redirect(url_for("download_file", name=filename))
+            return "File allowed!"
     return """
     <!doctype html>
     <title>Upload new File</title>
