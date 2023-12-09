90ac6b50cdb60779c9d45cdb738c3eace4ed469c
Max <m@xtyrrell.com>
2023-12-09 21:00:47 +0200

    fix: add /api/ prefix to routes

--- api/index.py
+++ api/index.py
@@ -3,45 +3,48 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-ALLOWED_EXTENSIONS = {'json'}
+ALLOWED_EXTENSIONS = {"json"}
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/api/")
 def home():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-@app.route('/about')
+
+@app.route("/api/about")
 def about():
-    return 'About'
+    return "About"
 
-@app.route('/generate-pdf')
+
+@app.route("/api/generate-pdf")
 def generate_pdf():
-    return 'generate pdf'
+    return "generate pdf"
+
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/uploads', methods=['GET', 'POST'])
+
+@app.route("/api/uploads", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
+    if request.method == "POST":
         # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
+        if "file" not in request.files:
+            flash("No file part")
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files["file"]
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
+        if file.filename == "":
+            flash("No selected file")
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
-    return '''
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return redirect(url_for("download_file", name=filename))
+    return """
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
@@ -49,5 +52,4 @@ def upload_file():
       <input type=file name=file>
       <input type=submit value=Upload>
     </form>
-    '''
-    
+    """
